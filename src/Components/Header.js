72ce5c4ecfb3af9e533988d61f5e31d06ae46114
gooseisdead <gooseisdead@mac.com>
2020-12-26 13:33:40 -0500

    4

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,20 @@
 import React from 'react' 
 import ElfName from '../Components/ElfName'
 
-// function Header(props) {
-
-//     let elf_roster = props.elves.map(function (elf_name) { return <ElfName name={elf_name} /> })
-
-//     return <div> 
-//                 <h1>ELF ROSTER</h1>
-//                 {elf_roster}
-//             </div>
-// }
-
 class Header extends React.Component {
-    render () {
 
-        let elf_roster = this.props.elves.map(function (elf_name) { return <ElfName name={elf_name} /> })
-        return <div> 
-                    <h1>ELF ROSTER</h1>
-                    {elf_roster}
-                </div>
-        }
+    elfRoster = () => {
+        return this.props.elves.map(function (elf_name) { return <ElfName name={elf_name} /> })
+    }
+
+    render () {
+        return (
+            <div> 
+                <h1>ELF ROSTER</h1>
+                {this.elfRoster()}
+            </div>
+        )
+    }
 }
 
 export default Header
