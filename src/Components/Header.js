943317d59710fae632566d0c5a8e4ef77cb78f8c
gooseisdead <gooseisdead@mac.com>
2020-12-29 10:56:51 -0500

    grease is the word

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,16 +3,20 @@ import ElfName from '../Components/ElfName'
 
 class Header extends React.Component {
 
-    state = { color: "blue" }
+    state = { color: "black" }
 
     elfRoster = () => {
         return this.props.elves.map(function (elf_name) { return <ElfName name={elf_name} /> })
     }
 
+    clickHandler = () => {
+        this.setState({ color: "blue" })
+    }
+
     render () {
         return (
             <div> 
-                <h1 style={{ "border-style": "dashed", "border-color": this.state.color }}>ELF ROSTER</h1>
+                <h1 onClick={this.clickHandler} style={{ "border-style": "dashed", "border-color": this.state.color }}>ELF ROSTER</h1>
                 {this.elfRoster()}
             </div>
         )
